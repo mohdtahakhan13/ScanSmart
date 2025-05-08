6fe5885fdd0dfbed635ce5a66cdfd1a821abf460
Taha Khan <mohdtahakhan13@gmail.com>
2025-05-08 17:59:52 +0530

    commit final

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,6 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
+    outDir: 'client/dist',
   },
 });
